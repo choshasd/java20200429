464daf5f5926ea713f8e2ccff1e2e5cdea951ed1
choshasd <choshasd@naver.com>
2020-04-29 11:19:41 +0900

    Q1_142page

---
+++ 20200429/src/Ex1.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class Calcu{
+	
+	//더하기메서드
+	public void doAdd(int a,int b) {
+		System.out.println(a + " + "+b+" = "+(a+b));
+	}
+
+	//빼기메서드
+	public void doSub(int a,int b) {
+		System.out.println(a + " - "+b+" = "+(a-b));
+	}
+
+	//곱하기메서드
+	public void doMul(int a,int b) {
+		System.out.println(a + " X "+b+" = "+(a*b));
+	}
+
+	//나누기메서드
+	public void doDiv(int a,int b) {
+		System.out.println(a + " / "+b+" = "+(a/b));
+	}
+
+	//나머지메서드
+	public void doMod(int a,int b) {
+		System.out.println(a + " % "+b+" = "+(a%b));
+	}
+}
+public class Ex1 {
+	public static void main(String[] args) {
+		dowhile:while(true) {
+			Calcu calcu = new Calcu();
+			Scanner aaa = new Scanner(System.in);
+			System.out.println("1.더하기 2.빼기 3.곱하기 4.나누기 5.나머지");
+			int select = aaa.nextInt();
+			System.out.println("숫자 한개 입력 : ");
+			int num1 = aaa.nextInt();
+			System.out.println("숫자 한개 입력 : ");
+			int num2 = aaa.nextInt();
+			
+			switch (select) {
+			case 1:
+				calcu.doAdd(num1,  num2);
+				break;
+			case 2:
+				calcu.doSub(num1, num2);
+				break;
+			case 3:
+				calcu.doMul(num1, num2);
+				break;
+			case 4:
+				calcu.doDiv(num1, num2);
+				break;
+			case 5:
+				calcu.doMod(num1, num2);
+				break;
+			default:
+				System.exit(1);
+				break dowhile;
+			}
+		}
+	}
+}
